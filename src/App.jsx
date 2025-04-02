fc0b9a02aa1caa66e80a44c34b48978cd0cbcbf9
Deniz <ddogru09@gmail.com>
2025-04-02 09:37:30 +0300

    player switch logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+import { useState } from 'react';
+
 import Player from './components/Player'
 import GameBoard from './components/GameBoard'
 
 function App() {
+    const [activePlayer, setActivePlayer] = useState('X')
+
+    function handleSelectSquare  () {
+        setActivePlayer((prevActivePlayer) => {
+            return prevActivePlayer === 'X' ? 'O' : 'X'
+        })
+    }
     return (
         <div id="game-container">
-            <ol id="players">
-                <Player name="player1" symbol="x" />
-                <Player name="player2" symbol="o" />
+            <ol id="players" className="highlight-player">
+                <Player name="player1" symbol="x" isActive={activePlayer === 'X'}/>
+                <Player name="player2" symbol="o" isActive={activePlayer === 'O'}/>
             </ol>
-            <GameBoard />
+            <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
         </div>
     );
 }
